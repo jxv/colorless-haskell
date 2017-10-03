b52013aa145f157167316e61752ee3fa8f97cb8f
jxv <joevargas92@gmail.com>
2017-10-03 01:42:52 -0700

    client: don't rexport unsafe functions

--- library/Colorless/Client.hs
+++ library/Colorless/Client.hs
@@ -6,4 +6,4 @@ module Colorless.Client
 
 import Colorless.Val
 import Colorless.Types
-import Colorless.Client.Expr
+import Colorless.Client.Expr hiding (unsafeExpr, unsafeRef, unsafeStmt, unsafePath)
