d7bea3443544769e9bd67fca4fca9556fb95b1a7
jxv <joevargas92@gmail.com>
2017-10-06 12:54:03 -0700

    update colorless imports

--- colorless/library/Colorless/Imports.hs
+++ colorless/library/Colorless/Imports.hs
@@ -4,10 +4,12 @@ module Colorless.Imports
   , module Data.Aeson
   , module Data.Text
   , module Data.Text.Conversions
+  , module Data.ByteString.Lazy.Char8
   ) where
 
 import Data.Map (Map, fromList, toList, empty, size)
 import Control.Monad.IO.Class (MonadIO(..))
 import Data.Aeson (ToJSON(..), FromJSON(..), object, (.=), (.:), Value(..), decode)
+import Data.ByteString.Lazy.Char8 (ByteString)
 import Data.Text (Text)
 import Data.Text.Conversions (ToText(..), FromText(..))
